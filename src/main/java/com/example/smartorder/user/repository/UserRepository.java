9384d60cc41e47c65d7695bb31e2973403797928
Jeon Hyewon <hw.jeon@tenqube.com>
2022-06-14 10:24:56 +0900

    Feature/3/sign up service (#4)
    * java version, jpa ddl-auto 변경
    
    resolves: #3
    
    * Join 서비스 구현
    
    resolves: #3
    
    * id 직접 생성 및 repository 연결 방식 변경
    
    resolves: #3

---
+++ src/main/java/com/example/smartorder/user/repository/UserRepository.java
@@ -0,0 +1,27 @@
+package com.example.smartorder.user.repository;
+
+import com.example.smartorder.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class UserRepository {
+    private final EntityManager em;
+
+    public void save(User user) {
+        this.em.persist(user);
+    }
+
+    public User findByAccessId(String accessId) {
+        List<User> results = this.em.createQuery("SELECT u FROM User u WHERE u.account.accessId = :accessId", User.class)
+                .setParameter("accessId", accessId)
+                .getResultList();
+        if (results.isEmpty()) return null;
+
+        return results.get(0);
+    }
+}
