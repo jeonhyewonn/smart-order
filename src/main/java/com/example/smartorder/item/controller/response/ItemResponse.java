3373a5f7d960eeb578a3f3eec4915d75f8533123
Jeon Hyewon <hw.jeon@tenqube.com>
2022-06-16 22:33:19 +0900

    feat: 전체 메뉴 조회 API 생성 (#20)
    - 전체 메뉴 조회 API는 인증 과정 생략
    
    resolves: #19

---
+++ src/main/java/com/example/smartorder/item/controller/response/ItemResponse.java
@@ -0,0 +1,17 @@
+package com.example.smartorder.item.controller.response;
+
+import com.example.smartorder.item.domain.Item;
+import lombok.Getter;
+
+@Getter
+public class ItemResponse {
+    private final String id;
+    private final String name;
+    private final Double price;
+
+    public ItemResponse(Item item) {
+        this.id = item.getId();
+        this.name = item.getName();
+        this.price = item.getPrice();
+    }
+}
