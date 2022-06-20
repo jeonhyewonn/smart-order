36b34803fafb97048f42a18c2a330f68182e8267
Jeon-Hyewon <hw.jeon@tenqube.com>
2022-06-20 19:12:04 +0900

    feat: controller 구현 - 주문, 조회, 취소 API
    resolves: #29

---
+++ src/main/java/com/example/smartorder/order/controller/dto/response/OrderItemResponse.java
@@ -0,0 +1,15 @@
+package com.example.smartorder.order.controller.dto.response;
+
+import com.example.smartorder.order.domain.OrderItem;
+import lombok.Getter;
+
+@Getter
+public class OrderItemResponse {
+    private final String id;
+    private final Integer quantity;
+
+    public OrderItemResponse(OrderItem orderItem) {
+        this.id = orderItem.getItem().getId();
+        this.quantity = orderItem.getQuantity();
+    }
+}
---
+++ src/main/java/com/example/smartorder/order/controller/dto/response/OrderResponse.java
@@ -0,0 +1,27 @@
+package com.example.smartorder.order.controller.dto.response;
+
+import com.example.smartorder.order.domain.Order;
+import com.example.smartorder.order.domain.OrderState;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@Getter
+public class OrderResponse {
+    private final List<OrderItemResponse> items;
+    private final Double payAmount;
+    private final Boolean isCanceled;
+    private final OrderState state;
+    private final LocalDateTime createdAt;
+
+    public OrderResponse(Order order) {
+        this.items = order.getOrderItems().stream().map(OrderItemResponse::new).collect(toList());
+        this.payAmount = order.getTotalAmount();
+        this.isCanceled = order.getIsCanceled();
+        this.state = order.getState();
+        this.createdAt = order.getCreatedAt();
+    }
+}
