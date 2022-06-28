02dc9405de3bac84c27c213f99e2b3432e434db8
Jeon-Hyewon <hw.jeon@tenqube.com>
2022-06-29 01:19:27 +0900

    fix: null/blank, nested valid 유효성 검사
    resolves: #39

--- src/main/java/com/example/smartorder/member/controller/dto/request/PasswordRequest.java
+++ src/main/java/com/example/smartorder/member/controller/dto/request/PasswordRequest.java
@@ -2,8 +2,13 @@ package com.example.smartorder.member.controller.dto.request;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 public class PasswordRequest {
+    @NotBlank
     private String oriPassword;
+
+    @NotBlank
     private String newPassword;
 }
