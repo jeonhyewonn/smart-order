01c308d7fb79cb3ff94361ce67434c3acf08d651
Jeon-Hyewon <hw.jeon@tenqube.com>
2022-06-30 16:42:02 +0900

    feat: OrderCookingService 생성 - 주문에 따라 재고 확인 및 차감
    resolves: #43

---
+++ src/main/java/com/example/smartorder/item/exception/InsufficientIngredientException.java
@@ -0,0 +1,5 @@
+package com.example.smartorder.item.exception;
+
+import com.example.smartorder.exception.CustomException;
+
+public class InsufficientIngredientException extends CustomException {}
--- src/test/java/com/example/smartorder/item/ItemMock.java
+++ src/test/java/com/example/smartorder/item/ItemMock.java
@@ -1,21 +1,42 @@
 package com.example.smartorder.item;
 
+import com.example.smartorder.item.domain.Ingredient;
 import com.example.smartorder.item.domain.Item;
+import com.example.smartorder.item.domain.ItemIngredient;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.springframework.test.util.ReflectionTestUtils.setField;
 
 public class ItemMock {
-    private static class TestItem extends Item {
-        public TestItem(Long id, String name, Double price) {
+    private static class TestIngredient extends Ingredient {
+        public TestIngredient() {
             super();
-            setField(this, "id", id);
-            setField(this, "name", name);
-            setField(this, "price", price);
+            setField(this, "id", 1L);
+            setField(this, "name", "유자청");
+            setField(this, "stock", 2);
             setField(this, "isDeleted", false);
-            setField(this, "createdAt", LocalDateTime.now());
         }
     }
-    public static final Item item = new TestItem(1L, "유자차", 5000.0);
+
+    private static class TestItemIngredient extends ItemIngredient {
+        public TestItemIngredient() {
+            super();
+            setField(this, "id", 1L);
+            setField(this, "ingredient", new TestIngredient());
+            setField(this, "isDeleted", false);
+        }
+    }
+
+    private static class TestItem extends Item {
+        public TestItem() {
+            super();
+            setField(this, "id", 1L);
+            setField(this, "name", "유자차");
+            setField(this, "price", 5000.0);
+            setField(this, "isDeleted", false);
+            setField(this, "itemIngredients", List.of(new TestItemIngredient()));
+        }
+    }
+    public final Item item = new TestItem();
 }
