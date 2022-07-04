70a4d185b1dd3a3959701a212fa4300796f30953
Jeon-Hyewon <hw.jeon@tenqube.com>
2022-07-04 23:14:31 +0900

    fix: queryDSL 설정 추가 및 전용 Repository 생성
    resolves: #50

--- src/main/java/com/example/smartorder/item/repository/ItemRepository.java
+++ src/main/java/com/example/smartorder/item/repository/ItemRepository.java
@@ -7,8 +7,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
-    @Override
+public interface ItemRepository extends JpaRepository<Item, Long>, ItemCustomRepository {
     @Query("SELECT i FROM Item i WHERE i.isDeleted = false")
     public List<Item> findAll();
 
