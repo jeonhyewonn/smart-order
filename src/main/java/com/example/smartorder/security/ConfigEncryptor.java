0b480b7864dbdc96ba4f03a42fa921d7f3dc46da
Jeon-Hyewon <hw.jeon@tenqube.com>
2022-07-05 14:54:17 +0900

    feat: jasypt 라이브러리 이용하여 ConfigEncryptor 생성
    resolves: #52

---
+++ src/main/java/com/example/smartorder/security/ConfigEncryptor.java
@@ -0,0 +1,35 @@
+package com.example.smartorder.security;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigEncryptor {
+    @Value("${jasypt.encryptor.password}")
+    private String password;
+
+    @Value("${jasypt.encryptor.algorithm}")
+    private String algorithm;
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(this.password);
+        config.setAlgorithm(this.algorithm);
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
+        config.setStringOutputType("base64");
+
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        encryptor.setConfig(config);
+
+        return encryptor;
+    }
+}
